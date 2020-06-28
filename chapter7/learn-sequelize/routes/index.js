cf600476f7b821e03d69d71d08056c02feb1dc26
김동엽 <yup@Yup-MacBookPro.local>
2020-06-28 15:37:13 +0900

    finish chap7 - DB usage example

--- chapter7/learn-sequelize/routes/index.js
+++ chapter7/learn-sequelize/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models').User;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try{
+    const users = await User.findAll();
+    console.log('start');
+    console.log(users);
+    res.render('sequelize', { users });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
--- chapter7/learn-sequelize/routes/users.js
+++ chapter7/learn-sequelize/routes/users.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models').User;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  try{
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const result = await User.create({
+      name: req.body.name,
+      age: req.body.age,
+      married: req.body.married,
+    });
+    console.log('sssssssssss');
+    console.log(result);
+    console.log(result.dataValues);
+    console.log('sssssssssss');
+    console.log(result.toJSON());
+    res.status(201).json(result);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
