f2aca95fe5924af54f2cef82b889352429ef2472
김동엽 <yup@Yup-MacBookPro.local>
2020-07-01 00:27:48 +0900

    finish chap.9

--- chapter9/nodebird/routes/page.js
+++ chapter9/nodebird/routes/page.js
@@ -1,26 +1,41 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middleware');
+const { Post, User } = require('../models');
 
-const router = express.Router();
+const router = new express.Router();
 
-router.get('/profile', (req, res) => {
-  res.render('profile', { title: '내 정보 - NodeBird', user: null });
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird', user: req.user });
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', {
     title: '회원가입 - NodeBird',
-    user: null,
+    user: req.user,
     joinError: req.flash('joinError'),
   });
 });
 
-router.get('/', (req, res, next) => {
-  res.render('main', {
-    title: 'NodeBird',
-    twits: [],
-    user: null,
-    loginError: req.flash('loginError'),
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+      user: req.user,
+      loginError: req.flash('loginError'),
+    });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+  
 });
 
 module.exports = router;
